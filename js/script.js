e8e5e5621be52c6d4dd293d7deb8d30d2051d138
urmilshroff <urmilshroff@gmail.com>
2018-12-07 11:56:38 +0530

    Added Allo to death row

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var deathTimeInbox = new Date("Mar 31, 2019 00:00:00").getTime();
+var deathTimeAllo = new Date("Mar 31, 2019 00:00:00").getTime();
 var deathTimeGooglePlus = new Date("Aug 31, 2019 00:00:00").getTime();
 
 var inbox = setInterval(function() {
@@ -11,10 +12,26 @@ var inbox = setInterval(function() {
 
     document.getElementById("inbox-countdown").innerHTML = "Inbox dead in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    if (distance < 0)
-    {
+    if (distance < 0) {
         clearInterval(inbox);
         document.getElementById("inbox-countdown").innerHTML = "R.I.P. Inbox, 2014-2019";
+    }
+
+}, 1000);
+
+var allo = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = deathTimeAllo - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("allo-countdown").innerHTML = "Allo dead in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(allo);
+        document.getElementById("allo-countdown").innerHTML = "R.I.P. Allo, 2016-2019";
     }
 
 }, 1000);
@@ -29,8 +46,7 @@ var googlePlus = setInterval(function() {
 
     document.getElementById("google-plus-countdown").innerHTML = "Google+ dead in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    if (distance < 0)
-    {
+    if (distance < 0) {
         clearInterval(googlePlus);
         document.getElementById("google-plus-countdown").innerHTML = "R.I.P. Google+, 2011-2019";
     }
