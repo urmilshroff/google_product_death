2ae0363e2751c71e125c0566f88176521ab78290
urmilshroff <urmilshroff@gmail.com>
2019-03-19 23:45:26 +0530

    R.I.P. Allo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var deathTimeInbox = new Date("Mar 31, 2019 00:00:00").getTime();
-var deathTimeAllo = new Date("Mar 31, 2019 00:00:00").getTime();
+// var deathTimeAllo = new Date("Mar 31, 2019 00:00:00").getTime();
 var deathTimeGooglePlus = new Date("April 2, 2019 00:00:00").getTime();
 
 var inbox = setInterval(function() {
@@ -19,22 +19,22 @@ var inbox = setInterval(function() {
 
 }, 1000);
 
-var allo = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = deathTimeAllo - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// var allo = setInterval(function() {
+//     var now = new Date().getTime();
+//     var distance = deathTimeAllo - now;
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("allo-countdown").innerHTML = "Allo dead in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+//     document.getElementById("allo-countdown").innerHTML = "Allo dead in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    if (distance < 0) {
-        clearInterval(allo);
-        document.getElementById("allo-countdown").innerHTML = "R.I.P. Allo, 2016-2019";
-    }
+//     if (distance < 0) {
+//         clearInterval(allo);
+//         document.getElementById("allo-countdown").innerHTML = "R.I.P. Allo, 2016-2019";
+//     }
 
-}, 1000);
+// }, 1000);
 
 var googlePlus = setInterval(function() {
     var now = new Date().getTime();
